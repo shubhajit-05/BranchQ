82dce02e2cb72df8cea76ca5ec72bf27781babd6
shubhajit-05 <shubhajitshaw5@gmail.com>
2026-09-23 14:43:12 +0530

    My first app

---
+++ android/app/src/main/kotlin/com/example/branch_q/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.branch_q
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_local_notifications
+import flutter_secure_storage_macos
+import geolocator_apple
+import local_auth_darwin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+}
